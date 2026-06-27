e96248dd5b7db882c637a190bc73d6d24069ce0e
kangwondaeho <ghkdlxm5901@kangwon.ac.kr>
2026-06-27 20:07:41 +0900

    add salvage item info display

---
+++ Assets/Scripts/ItemInfoDisplay.cs
@@ -0,0 +1,75 @@
+﻿using UdonSharp;
+using UnityEngine;
+using TMPro;
+
+public class ItemInfoDisplay : UdonSharpBehaviour
+{
+    public GameObject panelObject;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI priceText;
+    public TextMeshProUGUI decorateText;
+
+    private SalvageItem currentItem;
+
+    private void Start()
+    {
+        Hide();
+    }
+
+    public void ShowItem(SalvageItem item)
+    {
+        if (item == null)
+        {
+            Hide();
+            return;
+        }
+
+        currentItem = item;
+
+        if (panelObject != null)
+        {
+            panelObject.SetActive(true);
+        }
+
+        if (nameText != null)
+        {
+            nameText.text = item.itemName;
+        }
+
+        if (priceText != null)
+        {
+            priceText.text = "판매가: " + item.sellPrice;
+        }
+
+        if (decorateText != null)
+        {
+            if (item.canDecorate)
+            {
+                decorateText.text = "장식 가능";
+            }
+            else
+            {
+                decorateText.text = "판매 전용";
+            }
+        }
+
+        Debug.Log("[ItemInfoDisplay] 표시: " + item.itemName);
+    }
+
+    public void HideItem(SalvageItem item)
+    {
+        if (currentItem != item) return;
+
+        Hide();
+    }
+
+    public void Hide()
+    {
+        currentItem = null;
+
+        if (panelObject != null)
+        {
+            panelObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/SalvageItemInfoTrigger.cs
@@ -0,0 +1,43 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class SalvageItemInfoTrigger : UdonSharpBehaviour
+{
+    public SalvageItem item;
+    public ItemInfoDisplay display;
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        if (!player.isLocal) return;
+
+        if (item == null)
+        {
+            Debug.Log("[SalvageItemInfoTrigger] item이 연결되지 않음");
+            return;
+        }
+
+        if (display == null)
+        {
+            Debug.Log("[SalvageItemInfoTrigger] display가 연결되지 않음");
+            return;
+        }
+
+        if (item.isPlaced)
+        {
+            return;
+        }
+
+        display.ShowItem(item);
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        if (!player.isLocal) return;
+
+        if (display == null) return;
+        if (item == null) return;
+
+        display.HideItem(item);
+    }
+}
