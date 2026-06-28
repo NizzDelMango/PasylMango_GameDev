b855e3918790dfd038584b21d98e44a8d4fd5002
kangwondaeho <ghkdlxm5901@kangwon.ac.kr>
2026-06-28 17:53:21 +0900

    add exploration travel points

---
+++ Assets/Scripts/TravelPoint.cs
@@ -0,0 +1,30 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+public class TravelPoint : UdonSharpBehaviour
+{
+    public Transform targetPoint;
+    public string travelName = "이동";
+
+    public override void Interact()
+    {
+        if (targetPoint == null)
+        {
+            Debug.Log("[TravelPoint] targetPoint가 연결되지 않음: " + travelName);
+            return;
+        }
+
+        VRCPlayerApi localPlayer = Networking.LocalPlayer;
+
+        if (localPlayer == null)
+        {
+            Debug.Log("[TravelPoint] localPlayer를 찾을 수 없음");
+            return;
+        }
+
+        Debug.Log("[TravelPoint] 이동: " + travelName);
+
+        localPlayer.TeleportTo(targetPoint.position, targetPoint.rotation);
+    }
+}
